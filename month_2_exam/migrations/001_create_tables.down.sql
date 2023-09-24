7f44a60653c4f18e8ebee88979992730071cf168
jaloldinov <omadbekclever2@gmail.com>
2023-09-24 20:20:51 +0500

    down added

--- month_2_exam/migrations/001_create_tables.down.sql
+++ month_2_exam/migrations/001_create_tables.down.sql
@@ -1,3 +1,14 @@
+
+-- Drop foreign key constraints
+ALTER TABLE "remaining" DROP CONSTRAINT IF EXISTS "remaining_category_id_fkey";
+ALTER TABLE "remaining" DROP CONSTRAINT IF EXISTS "remaining_branch_id_fkey";
+ALTER TABLE "coming_table_product" DROP CONSTRAINT IF EXISTS "coming_table_product_coming_table_id_fkey";
+ALTER TABLE "coming_table_product" DROP CONSTRAINT IF EXISTS "coming_table_product_category_id_fkey";
+ALTER TABLE "coming_table" DROP CONSTRAINT IF EXISTS "coming_table_branch_id_fkey";
+ALTER TABLE "product" DROP CONSTRAINT IF EXISTS "product_category_id_fkey";
+ALTER TABLE "category" DROP CONSTRAINT IF EXISTS "category_parent_id_fkey";
+
+-- Drop tables
 DROP TABLE IF EXISTS "remaining";
 DROP TABLE IF EXISTS "coming_table_product";
 DROP TABLE IF EXISTS "coming_table";
